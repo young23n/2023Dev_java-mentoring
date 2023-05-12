6243525a6df65cf8d67d5f36b82c6c17e0b48db1
young23n <young23n@naver.com>
2023-05-12 13:04:35 +0900

    완료

--- src/com/iKeeper/Main.java
+++ src/com/iKeeper/Main.java
@@ -26,7 +26,7 @@ public class Main {
         int checkBCount = 0;
         Scanner scan = new Scanner(System.in);
 
-        void input(int money) {// 입력 받은 후 저장 (중복방지 구현필요)
+        void input(int money) {// 입력 받은 값 저장
             userNum = new ArrayList[money/1000];
             for (int i = 0; i < money / 1000; i++) {
                 userNum[i] = new ArrayList<Integer>();
@@ -37,23 +37,33 @@ public class Main {
             }
         }
 
-        void random() { // 랜덤 값 저장(당첨번호)
-            for (int i = 0; i <7; i++) {
+        void random() { // 랜덤 값 저장(당첨번호) + 중복방지
+            for (int i = 0; i < 7; i++) {//일단 다집어 넣음
                 winNum.add((int) (Math.random() * 99));
             }
+            for (int i = 0; i < 7; i++) {
+                 for(int k = 0; k < 7; k++){// 한개씩 비교 같은 인덱스는 제외
+                     if(winNum.get(i) == winNum.get(k) && i != k){
+                         winNum.set(i,(int) (Math.random() * 99));//다른 값으로 바꾸기
+                         k = 0;// 바뀐 값이 또 중복될 수 있기에 다시 탐색
+                     }
+                 }
+            }
         }
-        void checkNum(int money){//정답인 번호는 출력 후 지워버리고 보너스 번호 탐색
+
+
+        void checkNum(int money){//정답인 번호는 해당 리스트에서 지우기
             for(int i = 0; i < money / 1000; i++){
-                for(int j = 0; j  < 6; j++){//일반 체크
+                for(int j = 0; j  < 6; j++){//당첨 체크
                     if(userNum[i].get(j) == winNum.get(j)){
                         checkCount++;
                         userNum[i].remove(j);
                     }
                 }
             }
-            for(int i = 0; i < money / 1000; i++){
-                for(int k = 0; k < 6; k++){//보너스 체크
-                    if(userNum[i].get(k) == winNum.get(6)){
+            for(int i = 0; i < money / 1000; i++){// 지워지고 남은 값들 보너스번호 탐색
+                for(int k = 0; k < 6; k++){
+                    if(userNum[i].contains(winNum.get(6))){
                         checkBCount++;
                     }
                 }
@@ -70,7 +80,7 @@ public class Main {
             input(money);
             System.out.println("입력된 값: ");
 
-            for (ArrayList ar : userNum) {
+            for (ArrayList ar : userNum) {//장 마다 입력된 값들 출력
                 System.out.print(ar + " ");
                 System.out.println();
             }
@@ -78,20 +88,27 @@ public class Main {
 
             random();
             System.out.println("오늘 담첨번호는 ");
+
+            int cn = 0;// "보너스 번호:" 출력용
             for(Integer ar : winNum){
+                cn++;
+                if(cn == 7){//7번째 인덱스는 보너스 번호
+                    System.out.print("보너스 번호: ");
+                }
                 System.out.print(ar + " ");
             }
             System.out.println();
 
             checkNum(money);
             if(checkCount == 4 && checkBCount == 0){
+                System.out.println("4등입니다. 당첨금:100000");
 
             } else if (checkCount == 5 && checkBCount == 0) {
-
+                System.out.println("3등입니다. 당첨금:1000000");
             } else if (checkCount == 5 && checkBCount == 1) {
-
+                System.out.println("2등입니다. 당첨금:10000000");
             } else if (checkCount == 6 && checkBCount == 0) {
-
+                System.out.println("1등입니다. 당첨금:100000000");
             } else {
                 System.out.println("꽝입니다.");
             }
